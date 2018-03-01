a0e3181abc009883a75518bd4eaaaef791175bd3
Peggy Rayzis <peggyrayzis@gmail.com>
2018-03-01 13:03:05 -0500

    Another attempt at Placeholder :(

--- src/Placeholder.js
+++ src/Placeholder.js
@@ -3,12 +3,14 @@ import React, { Fragment } from "react";
 function Placeholder({ delayMs, fallbackUI, children }) {
 	return (
 		<React.Timeout ms={delayMs}>
-			{didTimeout => (
-				<Fragment>
-					<div hidden={didTimeout}>{children}</div>
-					{didTimeout ? fallbackUI : null}
-				</Fragment>
-			)}
+			{didTimeout => {
+				return (
+					<Fragment>
+						<span hidden={didTimeout}>{children}</span>
+						{didTimeout ? fallbackUI : null}
+					</Fragment>
+				);
+			}}
 		</React.Timeout>
 	);
 }
